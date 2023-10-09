ad220ca14752a2ed7a10bc63eeab001a30f699b2
Winfried Baumann <frieder@holoride.com>
2023-10-09 14:57:00 +0200

    too much..

--- winget-updater-library/src/lib.rs
+++ winget-updater-library/src/lib.rs
@@ -64,7 +64,7 @@ pub mod wud {
         let result = String::from_utf8_lossy(&winget_output.stdout);
         let lines: Vec<_> = result.lines().collect();
         let last_line = lines.iter().rposition(|&x| x.trim().is_empty()).expect("Couldn't find the empty line that is usually printed after the package listing");
-        let packages_to_update = lines[2..last_line].into_iter().map(|x| x.to_string()).collect();
+        let packages_to_update = lines[2..last_line-1].into_iter().map(|x| x.to_string()).collect();
         packages_to_update
     }
     
